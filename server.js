39be4ecfeaa12a20aa565bd9505ae0ab5793ff44
Vivek Mobiuso <vivek@mobiuso.com>
2024-01-02 10:10:18 +0530

    Added cors

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const connectDB = require("./config/db.js");
 const userRoutes = require("./routes/userRoutes.js");
 const chatRoutes = require("./routes/chatRoutes.js");
 const messageRoutes = require("./routes/messageRoutes.js");
+const cors = require("cors");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware.js");
 
 dotenv.config();
-
+app.use(cors());
 connectDB();
 const app = express()
 app.use(express.json())
@@ -21,12 +22,6 @@ app.use("/api/chat", chatRoutes)
 app.use("/api/message", messageRoutes)
 
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
 
 app.use(notFound);
 app.use(errorHandler);
